5fbd90b98a75de2bb556b6c5a4963f3ebe108ef0
Matthew DiLoreto <mjdiloreto@gmail.com>
2020-06-29 20:25:24 +0000

    Begin trying out pattern on a single svg element, and expand functionality of grid app.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,13 +1,23 @@
 (ns app.core
   (:require [reagent.core :as r]
+            [textures]
+            [d3-selection]
             [reagent.dom :as rdom]
             [app.views :as views]))
 
 (defn ^:dev/after-load start
   []
-  (rdom/render [views/app] (.getElementById js/document "app")))
+  (rdom/render [views/simpler] (.getElementById js/document "app")))
 
 (defn ^:export main
   []
-  (start))
+  (let
+      [texture (.thicker (.lines textures))
+       example-svg-elt (.select d3-selection "#example")]
+    (do (.call example-svg-elt texture)
+        (.style (.append example-svg-elt "circle")
+                "fill"
+                (.url texture))
+        [:div "hello world"]
+        #_(start))))
 
--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,14 +1,18 @@
 (ns app.views
   (:require [reagent.core :refer [atom]]
+            [textures]
+            [d3-selection]
             [app.state :refer [app-state]]
             [app.events :refer [increment decrement]]))
 
 (def w (atom 10))
+(defn swap-w [e] (swap! w #(.-value (.-target e))))
 (def h (atom 10))
-(def state (atom (vec (repeat @w (vec (repeat @h "red"))))))
+(defn swap-h [e] (swap! h #(.-value (.-target e))))
+(def state (atom {}))
 
 (defn pixel-state [x y] (get-in @state [x y]))
-(defn swap-pixel [x y val] (swap! state assoc-in [x y] val))
+(defn swap-pixel [x y val] (swap! state assoc [x y] val))
 
 (defn pixel
   [x y options]
@@ -16,25 +20,40 @@
    {:on-click #(swap-pixel x y "blue")
     :style {:width "20px"
             :height "20px"
-            :background (pixel-state x y)
+            :background (if-let [p (pixel-state x y)]
+                          p
+                          "red")
             :outline (when (:outline options) "2px solid black")}}])
 
-(defn pixel-grid2
+(defn pixel-grid
   [w h options]
   (into [:div]
         (for [x (range w)]
           [:span {:style {:float "left"}}
            (for [y (range h)]
-             [pixel x y options])])))
+             ^{:key [x y]} [pixel x y options])])))
 
-(defn pixel-grid
-  [w h options]
-  (into [:div]
-        (repeat w
-                (into [:span {:style {:float "left"}}]
-                      (repeat h [pixel options])))))
+(defn width-slider
+  [w]
+  [:div
+   "Change width"
+   [:input {:type "range" :min 1 :max 100 :value w :on-change #(swap-w %)}]])
+
+(defn height-slider
+  [h]
+  [:div
+   "Change height"
+   [:input {:type "range" :min 1 :max 100 :value h :on-change #(swap-h %)}]])
 
 (defn app []
   [:div
-   [pixel-grid2 10 10 {:outline true}]])
+   [pixel-grid @w @h {:outline true}]
+   [width-slider @w]
+   [height-slider @h]])
+
+
+
+(defn simpler
+  []
+  )
 
